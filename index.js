731b101a994109f6aac692402e49bc35c846db54
Emily Fernschild <emilyfernschild@gmail.com>
2022-08-30 21:07:57 -0400

    commit tree 148e3f6d7e55fca2f78ed7dc3a50601c65c37c83

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+/*function superbowlWin(record) {
+    if (result === "W"){
+     return record.find(superbowlWin(year));
+    }
+    else if (result === "N/A")
+    return undefined;
+}*/
+
+function superbowlWin(record){
+    for(let item of record){
+        if(item.result === "W"){
+            return item.year;
+        }
+    }
+}
